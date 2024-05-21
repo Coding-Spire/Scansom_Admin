7ec13369500cc2976181dd7597751298b1a8617c
Venkatraman Nagarajan <reachtovenkatraman.gmail.com>
2024-05-21 12:13:20 +0530

    Added Login with FB

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,13 +5,15 @@ let { VITE_FIREBASE_CONFIG, NODE_ENV } = import.meta.env;
 
 const firebaseConfig: FirebaseOptions = VITE_FIREBASE_CONFIG
   ? JSON.parse(VITE_FIREBASE_CONFIG)
-  : { apiKey: 'MOCK_KEY' };
+  : { apiKey: 'AIzaSyCgmmVSDo1t7knIe3mmjbf0XsIIgpbp2fI',
+      authDomain: "sacnsomadmin.firebaseapp.com"
+   };
 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
 export const firebaseAuth = getAuth(app);
 
-if (NODE_ENV !== 'production') {
-  connectAuthEmulator(firebaseAuth, 'http://localhost:9099');
-}
+// if (NODE_ENV !== 'production') {
+//   connectAuthEmulator(firebaseAuth, 'http://localhost:9099');
+// }
